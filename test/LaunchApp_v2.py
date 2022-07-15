78114280024242abbe415e8b19baaf18798789e3
ThuPham <ptthu36@gmail.com>
2022-07-15 18:24:31 +0700

    Update python script intp directory

--- test/LaunchApp_v2.py
+++ test/LaunchApp_v2.py
@@ -5,15 +5,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_launchApp(host="http://localhost:4723/wd/hub", udid="K6F1021084B0061", **kwargs):
+def launchApp(host="http://localhost:4723/wd/hub", udid="K6F1021084B0061", **kwargs):
     desired_caps = {'udid': udid, 'platformName': 'android', 'automationName': 'UiAutomator2'}
     desired_caps.update(kwargs)
     return webdriver.Remote(host, desired_caps)
 
 
 if __name__ == '__main__':
-    test_launchApp(
+    driver = launchApp(
         app="/Users/pthanh2/Documents/AUTOMATION/0_HADO_FOSSIL/0_CODE/pythonThuPhamMobile/app/com.nike.plusgps.apk",
         appPackage="com.nike.plusgps", appActivity="com.nike.plusgps.activities.MainActivity")
-
+    time.sleep(10)
+    driver.quit()
     # launchApp(app="/Users/pthanh2/Documents/AUTOMATION/0_HADO_FOSSIL/0_CODE/pythonThuPhamMobile/app/calculator.apk")
